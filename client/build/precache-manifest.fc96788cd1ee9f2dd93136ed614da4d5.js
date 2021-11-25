dfc33fcd9079db040414778bfdc3ab6c6c9818a4
David Tierney <daveshouse44@hotmail.com>
2021-11-24 18:59:52 -0600

    added build folder, modified gitignored

---
+++ client/build/precache-manifest.fc96788cd1ee9f2dd93136ed614da4d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61672f0faf01a1aeadcbbea0ee7d47c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3d363240854e31c341d",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "60048d0c8dc78b951af9",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "f3d363240854e31c341d",
+    "url": "/static/js/2.d5e016ae.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.d5e016ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60048d0c8dc78b951af9",
+    "url": "/static/js/main.5295ad46.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
