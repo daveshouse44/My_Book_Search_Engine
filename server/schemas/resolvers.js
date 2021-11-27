65ba3e9a72ed91af984d616e427cbbd05c779435
David Tierney <daveshouse44@hotmail.com>
2021-11-27 02:57:58 -0600

    tinkering with authentication, rebuild & redeploy

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,15 +32,14 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
-
       return { token, user };
     },
     saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
+        const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
           { $addToSet: { savedBooks: bookData } },
           { new: true, runValidators: true }
